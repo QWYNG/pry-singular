023c8a76a11a9acd570da1ea7828fdbc0e24228a
QWYNG <ikusawasi@gmail.com>
2020-03-16 22:49:39 +0900

    adjust standardrb update

--- test/slop_test.rb
+++ test/slop_test.rb
@@ -3,7 +3,6 @@ require_relative "test_helper"
 class PrySingular::SlopTest < Minitest::Test
   def test_parse_singular_method_command
     command_entered = "create :user, :super, name: 'Thresh'"
-    assert_equal("create :user,:super,name:'Thresh'",
-                 PrySingular::Slop.parse_singular_method_command(command_entered))
+    assert_equal("create :user,:super,name:'Thresh'", PrySingular::Slop.parse_singular_method_command(command_entered))
   end
 end
